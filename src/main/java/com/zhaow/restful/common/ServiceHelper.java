5a2459d20e2094317943f07aa53cb86861c7cba9
ALongo <ALongo@tl.com>
2026-06-17 13:05:54 +0800

    :fire: 移除 Services 树窗口功能
    - 删除整个 navigator 包（RestServicesNavigator、RestServiceStructure、
      RestServicesNavigatorPanel、RestServiceDetail 等 12 个类 + form）
    - 删除 utils/RestServiceDataKeys
    - 精简 ServiceHelper（移除仅供树视图使用的 buildRestServiceProjectListUsingResolver）
    - 清理 plugin.xml（project-components、projectListeners、树工具栏/菜单 action）

--- src/main/java/com/zhaow/restful/common/ServiceHelper.java
+++ src/main/java/com/zhaow/restful/common/ServiceHelper.java
@@ -2,14 +2,12 @@ package com.zhaow.restful.common;
 
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiMethod;
 import com.zhaow.restful.common.resolver.JaxrsResolver;
 import com.zhaow.restful.common.resolver.ServiceResolver;
 import com.zhaow.restful.common.resolver.SpringResolver;
 import com.zhaow.restful.navigation.action.RestServiceItem;
-import com.zhaow.restful.navigator.RestServiceProject;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -26,28 +24,13 @@ public class ServiceHelper {
         this.psiMethod = psiMethod;
     }
 
-    public static List<RestServiceProject> buildRestServiceProjectListUsingResolver(Project project) {
-//        System.out.println("buildRestServiceProjectList");
-        List<RestServiceProject> serviceProjectList = new ArrayList<>();
-
-        Module[] modules = ModuleManager.getInstance(project).getModules();
-        for (Module module : modules) {
-            List<RestServiceItem> restServices = buildRestServiceItemListUsingResolver(module);
-            if (restServices.size() > 0) {
-                serviceProjectList.add(new RestServiceProject(module, restServices));
-            }
-        }
-
-        return serviceProjectList;
-    }
-
     public static List<RestServiceItem> buildRestServiceItemListUsingResolver(Module module) {
 
         List<RestServiceItem> itemList = new ArrayList<>();
 
         SpringResolver springResolver = new SpringResolver(module);
         JaxrsResolver jaxrsResolver = new JaxrsResolver(module);
-        ServiceResolver[] resolvers = {springResolver,jaxrsResolver};
+        ServiceResolver[] resolvers = {springResolver, jaxrsResolver};
 
         for (ServiceResolver resolver : resolvers) {
             List<RestServiceItem> allSupportedServiceItemsInModule = resolver.findAllSupportedServiceItemsInModule();
@@ -65,7 +48,7 @@ public class ServiceHelper {
         SpringResolver springResolver = new SpringResolver(project);
         JaxrsResolver jaxrsResolver = new JaxrsResolver(project);
 
-        ServiceResolver[] resolvers = {springResolver,jaxrsResolver};
+        ServiceResolver[] resolvers = {springResolver, jaxrsResolver};
         for (ServiceResolver resolver : resolvers) {
             List<RestServiceItem> allSupportedServiceItemsInProject = resolver.findAllSupportedServiceItemsInProject();
 
